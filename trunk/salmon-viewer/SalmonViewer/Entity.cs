8f925d0fcb0e4a4b268f1a129a05d1c9ef01861c
scott.ellington <scott.ellington@8a4271af-0640-0410-ad9a-139c3939e831>
2009-11-12 16:09:27 +0000

    * salmon-viewer.sln: * SalmonViewer/Entity.cs: * SalmonViewer/ThreeDSFile.cs: fix issue #2

--- trunk/salmon-viewer/SalmonViewer/Entity.cs
+++ trunk/salmon-viewer/SalmonViewer/Entity.cs
@@ -23,6 +23,8 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
+using System.Collections.Generic;
+
 using Tao.OpenGl;
 
 namespace SalmonViewer
@@ -39,6 +41,12 @@ namespace SalmonViewer
 		}
 	}
 	
+	public class MaterialFaces
+	{
+		public Material Material;
+		public ushort[] Faces;
+	}
+	
 	public class Entity : IRenderable
 	{
 		// TODO: OO this
@@ -46,9 +54,12 @@ namespace SalmonViewer
 		// constructor with verts and faces
 		// normalize in ctor
 		
+		#region vars
+		
 		int glListNo = -1;
 		
-		public Material material = new Material ();
+		//public Material material = new Material ();
+		List<MaterialFaces> materialFaces = new List<MaterialFaces>();
 		
 		// The stored vertices 
 		public Vector[] vertices;
@@ -59,13 +70,20 @@ namespace SalmonViewer
 		// The indices of the triangles which point to vertices
 		public Triangle[] indices;
 
-
-		public Quad[] quads;
-		
 		// The coordinates which map the texture onto the entity
 		public TexCoord[] texcoords;
 	
 		bool normalized = false;
+
+		public List<MaterialFaces> MaterialFaces {
+					get {
+						return materialFaces;
+					}
+				}	
+		
+		#endregion
+		
+		#region public methods
 		
 		/// <summary>
 		/// Calculates the surface normals for all vertices.
@@ -131,47 +149,66 @@ namespace SalmonViewer
 			}
 			
 			// first run we create the glList and manually render our entity
-			
 			glListNo = Gl.glGenLists(1);
 			Gl.glNewList(glListNo, Gl.GL_COMPILE);
 
-			// set up light properties
-			Gl.glMaterialfv (Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, material.Ambient);
-			Gl.glMaterialfv (Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, material.Diffuse);
-			Gl.glMaterialfv (Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, material.Specular);
-			Gl.glMaterialf (Gl.GL_FRONT_AND_BACK, Gl.GL_SHININESS, material.Shininess);
-	
-			// if we have a texture, bind it
-			if (material.TextureId >= 0 ) 
+			// some 3ds files have no material faces, so we just draw all the faces
+			bool noMaterials = materialFaces.Count == 0;
+			if (noMaterials)
 			{
-				Gl.glBindTexture ( Gl.GL_TEXTURE_2D, material.TextureId ); 
-				Gl.glEnable( Gl.GL_TEXTURE_2D );
-			}
-
-			// Draw every triangle in the entity, it's normal, and it's material (if exists)
-			Gl.glBegin ( Gl.GL_TRIANGLES);		
-			foreach ( Triangle tri in indices )
-			{ 
-				// Vertex 1
-				if (normalized) Gl.glNormal3d ( normals[tri.Vertex1].X, normals[tri.Vertex1].Y, normals[tri.Vertex1].Z );
-				if ( material.TextureId >= 0 ) Gl.glTexCoord2f ( texcoords [ tri.Vertex1 ].U, texcoords [ tri.Vertex1 ].V);
-				Gl.glVertex3d ( vertices[tri.Vertex1].X, vertices[tri.Vertex1].Y, vertices[tri.Vertex1].Z );
-
-				// Vertex 2
-				if (normalized) Gl.glNormal3d ( normals[tri.Vertex2].X, normals[tri.Vertex2].Y, normals[tri.Vertex2].Z );
-				if ( material.TextureId >= 0 ) Gl.glTexCoord2f ( texcoords [ tri.Vertex2 ].U, texcoords [ tri.Vertex2 ].V);
-				Gl.glVertex3d ( vertices[tri.Vertex2].X, vertices[tri.Vertex2].Y, vertices[tri.Vertex2].Z );
-
-				// Vertex 3
-				if (normalized) Gl.glNormal3d ( normals[tri.Vertex3].X, normals[tri.Vertex3].Y, normals[tri.Vertex3].Z );
-				if ( material.TextureId >= 0 ) Gl.glTexCoord2f( texcoords [ tri.Vertex3 ].U, texcoords [ tri.Vertex3 ].V);
-				Gl.glVertex3d ( vertices[tri.Vertex3].X, vertices[tri.Vertex3].Y, vertices[tri.Vertex3].Z );
+				MaterialFaces m = new MaterialFaces();
+				m.Material = new Material();
+				materialFaces.Add(m);	
 			}
 			
-			Gl.glEnd();
+			foreach (MaterialFaces m in materialFaces)
+			{
+				Material material = m.Material;
+			
+				// set up light properties
+				Gl.glMaterialfv (Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, material.Ambient);
+				Gl.glMaterialfv (Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, material.Diffuse);
+				Gl.glMaterialfv (Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, material.Specular);
+				Gl.glMaterialf (Gl.GL_FRONT_AND_BACK, Gl.GL_SHININESS, material.Shininess);
+		
+				// if we have a texture, bind it
+				if (material.TextureId >= 0 ) 
+				{
+					Gl.glBindTexture ( Gl.GL_TEXTURE_2D, material.TextureId ); 
+					Gl.glEnable( Gl.GL_TEXTURE_2D );
+				}
+	
+				// Draw every triangle in the entity, it's normal, and it's material (if exists)
+				Gl.glBegin ( Gl.GL_TRIANGLES);		
+					
+				//foreach ( Triangle tri in indices )
+				for (int ii = 0; ii < (noMaterials ? indices.Length : m.Faces.Length); ii++)
+				{ 
+					Triangle tri = noMaterials ? indices[ii] : indices[m.Faces[ii]];
+					
+					// Vertex 1
+					if (normalized) Gl.glNormal3d ( normals[tri.Vertex1].X, normals[tri.Vertex1].Y, normals[tri.Vertex1].Z );
+					if ( material.TextureId >= 0 ) Gl.glTexCoord2f ( texcoords [ tri.Vertex1 ].U, texcoords [ tri.Vertex1 ].V);
+					Gl.glVertex3d ( vertices[tri.Vertex1].X, vertices[tri.Vertex1].Y, vertices[tri.Vertex1].Z );
+	
+					// Vertex 2
+					if (normalized) Gl.glNormal3d ( normals[tri.Vertex2].X, normals[tri.Vertex2].Y, normals[tri.Vertex2].Z );
+					if ( material.TextureId >= 0 ) Gl.glTexCoord2f ( texcoords [ tri.Vertex2 ].U, texcoords [ tri.Vertex2 ].V);
+					Gl.glVertex3d ( vertices[tri.Vertex2].X, vertices[tri.Vertex2].Y, vertices[tri.Vertex2].Z );
+	
+					// Vertex 3
+					if (normalized) Gl.glNormal3d ( normals[tri.Vertex3].X, normals[tri.Vertex3].Y, normals[tri.Vertex3].Z );
+					if ( material.TextureId >= 0 ) Gl.glTexCoord2f( texcoords [ tri.Vertex3 ].U, texcoords [ tri.Vertex3 ].V);
+					Gl.glVertex3d ( vertices[tri.Vertex3].X, vertices[tri.Vertex3].Y, vertices[tri.Vertex3].Z );
+				}
+				Gl.glEnd();
+			}
+			
 			Gl.glDisable( Gl.GL_TEXTURE_2D );
 			
 			Gl.glEndList();
 		}	
+		
+		#endregion
 	}
 }
